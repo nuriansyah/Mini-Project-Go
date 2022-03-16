31803ab2a39b409ee8d8754046c8caf378c55eab
nuriansyah <nuriansyah09@gmail.com>
2022-03-16 15:02:41 +0700

    Update: main.go

--- main.go
+++ main.go
@@ -1,31 +1,20 @@
 package main
 
-import "fmt"
-
-//"log"
-//"net/http"
-//"learn-go/handler"
+import (
+	"log"
+	"mini-project-go/handler"
+	"net/http"
+)
 
 func main() {
-	/* mux := http.NewServeMux()
+	mux := http.NewServeMux()
 
-
-
-	mux.HandleFunc("/hello",handler.HelloHandler)
-	mux.HandleFunc("/product",handler.ProductHandler)
-	mux.HandleFunc("/",handler.HomeHandler)
+	mux.HandleFunc("/hello", handler.HelloHandler)
+	mux.HandleFunc("/product", handler.ProductHandler)
+	mux.HandleFunc("/", handler.HomeHandler)
 	log.Println("Starting port 8080")
 
 	err := http.ListenAndServe(":8080", mux)
-	log.Fatal(err) */
+	log.Fatal(err)
 
-	scores := []int{8, 9, 9, 9, 10, 6, 5, 7, 3, 4, 5}
-
-	var total int
-
-	for _, score := range scores {
-		total += score
-	}
-
-	fmt.Println(total)
 }
